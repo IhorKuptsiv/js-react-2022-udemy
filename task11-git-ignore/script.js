2035526ec19a1f5c57ec08fb7f58135e3de7642a
Ihor <cronosgothic@gmail.com>
2022-06-01 11:54:45 +0300

    add git pull test

---
+++ task11-git-ignore/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// https://drive.google.com/file/d/1XjU5Bv9B795eQa2c0rfg_Hr11sFvWwgh/view
+// https://www.gitkraken.com/
+
+//git clone URL - скачує проект
+
+// git pull - скачує зміни !!!
+
+
+
