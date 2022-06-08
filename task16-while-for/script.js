35b86e2fbc522feff974e2fbab1c57811f4b18e1
Ihor <cronosgothic@gmail.com>
2022-06-08 14:28:32 +0300

    for and while

---
+++ task16-while-for/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// цикли for /while
+
+let num = 50;
+
+// поки num менше 77 ми виконуємо дію
+//while(num <= 55){
+//console.log(num);
+//num++;
+//}
+
+//do{
+//    console.log(num);
+//    num++;
+//}
+//while (num <55);
+
+//for(let i = 1; i < 8; i++){
+//    console.log(num);
+//    num++;
+//}
+
+for( let i = 1; i < 10; i++){
+   if (i === 6){
+      //брейл викидає на 5
+      // break;
+      // контіню пропускає 6 і іде далі
+      continue;
+   }
+    console.log(i);
+}
