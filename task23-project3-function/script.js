1baae1ed65a68dbd579cef03296b6edbbc1fef8f
Ihor <cronosgothic@gmail.com>
2022-06-09 23:56:46 +0300

    function

---
+++ task23-project3-function/script.js
@@ -0,0 +1,46 @@
+"use strict";
+// Код возьмите из предыдущего домашнего задания
+
+const numberOfFilms = +prompt("Скільки фільмів ви переглянули?","");
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+//1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+for(let i = 0; i < 2; i++){
+    const a = prompt("Один з останніх фільмів які переглядали?",""),
+    b = +prompt("На скільки оціните його?","");
+//2) Сделать так, чтобы пользователь не мог оставить 
+//ответ в виде пустой строки,отменить ответ или ввести
+//название фильма длинее, чем 50 символов. 
+//Если это происходит - возвращаем пользователя к вопросам опять
+if(a != null && b !=null && a != '' && b != '' && a.length < 50){
+    personalMovieDB.movies [a] = b;
+    console.log('done');
+}else{
+    console.log('error');
+    i--;
+}
+}
+// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+if(personalMovieDB.count < 10){
+console.log( "Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count >=10 && personalMovieDB.count < 30){
+console.log ("Вы классический зритель");
+}else if (personalMovieDB.count >=30){
+console.log("Вы киноман");
+}else{
+console.log("Произошла ошибка");
+}
+
+
+
+console.log(personalMovieDB);
