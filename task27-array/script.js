41a8f5c3931a2d478287c9bc83191781b667e733
Ihor <cronosgothic@gmail.com>
2022-06-15 19:23:09 +0300

    split join sort

--- task27-array/script.js
+++ task27-array/script.js
@@ -3,10 +3,16 @@
 //http://coldfox.ru/article/5c7ffe64bbf20e61c12c7348/%D0%9E%D1%82%D0%BB%D0%B8%D1%87%D0%B8%D0%B5-for-of-%D0%BE%D1%82-for-in-%D0%B2-javascript
 
 
-const arr = [2,3,6,8,10];
+const arr = [2,13,26,8,10];
 // arr[99] = 0;
 // console.log(arr.length);
 // console.log(arr);
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b){
+    return a - b;
+}
 
 arr.forEach(function(item,i,arr){
     //item-значення кожного елемент  і - індекс кожног. елем, arr - масив
@@ -24,7 +30,12 @@ console.log(`${i}: ${item} в середині масиву ${arr}`);
 // for( let i = 0; i < arr.length; i++){
 //     console.log(arr[i]);
 // }
-//перебор масиву
-for( let value of arr){
-    console.log(value);
-}
+//перебор масиву break continue - можна використов. 
+// for( let value of arr){
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
