4acb3fa54656e6921bbe808e5b6a6af6b2db85ab
Ihor <cronosgothic@gmail.com>
2022-06-16 20:19:40 +0300

    Object create and setprototypeOf

--- task30-oop/script.js
+++ task30-oop/script.js
@@ -11,11 +11,24 @@ console.dir([1,2,3]);
 
 const soldier = {
       health: 400,
-      armor: 100
+      armor: 100,
+      sayHello: function() {
+        console.log("hello");
+      }
 };
-const john = {
-    health: 100
-};
+//створюєм новий обєкт джон який буде прототипно наслідуватись від солдата
+//щоб джон мав доступ до функцій, методів, свойств
+const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
+// };
 // старий формат прототипів  джон візьме армор з солдата
-john.__proto__= soldier;
-console.dir(john.armor);
+//john.__proto__= soldier;
+
+//Object.setPrototypeOf(john, soldier);
+
+//console.dir(john.armor);
+john.sayHello();
+
+
