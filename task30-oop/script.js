0badbd00e8a20a48b59ccab25653699fe49860a0
Ihor <cronosgothic@gmail.com>
2022-06-16 19:56:27 +0300

    proto

--- task30-oop/script.js
+++ task30-oop/script.js
@@ -4,5 +4,18 @@
 let str = "some";
 let strObj = new String(str);
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+      health: 400,
+      armor: 100
+};
+const john = {
+    health: 100
+};
+// старий формат прототипів  джон візьме армор з солдата
+john.__proto__= soldier;
+console.dir(john.armor);
