7812935a99b0a706d10467c62c1c28b169e59437
Ihor <cronosgothic@gmail.com>
2022-06-21 20:13:36 +0300

    closure

--- task34-closure/script.js
+++ task34-closure/script.js
@@ -4,3 +4,12 @@
 //https://learn.javascript.ru/function-object
 // Замикання функцій та лексичне оточення
 
+let number = 5;
+
+function logNumber() {
+  // number - ссилка на змінну, а не її значення(5) = 6
+  console.log(number);
+}
+
+number = 6;
+logNumber();
