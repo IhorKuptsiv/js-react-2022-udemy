0bdd4b3e0c1bf80e1b91a1dee523d3777ca822f2
Ihor <cronosgothic@gmail.com>
2022-06-22 20:30:52 +0300

    DOM, getElementById, querySelector

--- task36-elements-from-page/js/script.js
+++ task36-elements-from-page/js/script.js
@@ -15,3 +15,22 @@ console.log(box);
 const btns = document.getElementsByTagName('button')[1];
 console.log(btns);
 // або console.log(btns[1]);
+
+
+// получаємо всі елементи через клас circle 
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+// метод querySelectorAll - в середину () вставляємо CSS селектор
+//Підтримує - #id, class, вложеність класів, псевдокласи, атрибути класу
+const hearts = document.querySelectorAll('.heart');//клас .heart - коли селектор ставимо . при вказуванні класу
+
+//console.log(hearts);
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+//querySelector -  позволяє плучити тільки 1 елемент зі сторінки
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
