5745207215a141bbe71c98d60b9ff62bd6830b49
Ihor <cronosgothic@gmail.com>
2022-06-22 23:05:03 +0300

    page elements

---
+++ task37-page-element/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+//https://drive.google.com/file/d/1sxbFDM645vAVnBhUuSgGKXadJTKuIn28/view
+//https://caniuse.com/
+
+const box = document.getElementById('box');
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('heart'),
+      oneHeart = document.querySelector('.heart');
+      
+
