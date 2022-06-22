66866258ca12f280602269deab9d44a613175b8c
Ihor <cronosgothic@gmail.com>
2022-06-22 20:13:59 +0300

    htmlcollection

--- task36-elements-from-page/js/script.js
+++ task36-elements-from-page/js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+//https://habr.com/ru/post/336136/
+
+const box = document.getElementById('box');//id
+
+console.log(box);
+
+// в змінній btns получаємо псевдомасив ВСІХ button
+//const btns = document.getElementsByTagName('button');
+//результат html колекція
+//console.log(btns);
+// щоб отримати певну кнопку вказати потрібно індекс кнопки
+// індекс 1 - 2 кнопка
+const btns = document.getElementsByTagName('button')[1];
+console.log(btns);
+// або console.log(btns[1]);
