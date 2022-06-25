f1e2a34128fc87f0c8826cf3a60f9fcdc1a47341
Ihor <cronosgothic@gmail.com>
2022-06-26 02:38:04 +0300

    event mobile touch

--- task43-event-in-phone/script.js
+++ task43-event-in-phone/script.js
@@ -1,2 +1,43 @@
 //https://habr.com/ru/company/sibirix/blog/227175/
 'use strict';
+
+// touchstart - при дотику
+// touchmove - коли зміщується палець
+// touchend -  коли відірвався палець
+// touchenter - коли палиць заходить на границі елементи
+// touchleave - коли вийшов за межі елементу
+// touchcancel - коли дотику не рєєструється на екрані
+
+window.addEventListener('DOMContentLoaded', () => {
+const box = document.querySelector ('.box');
+
+box.addEventListener('touchstart', (e) =>{
+  e.preventDefault();
+
+  console.log('Start');
+  //console.log(e.touches);
+  console.log(e.targetTouches);
+  //console.log(e.changedTouches);
+
+  
+
+  
+
+});
+box.addEventListener('touchmove', (e) =>{
+  e.preventDefault();
+
+  console.log(e.targetTouches[1].pageX);
+});
+
+box.addEventListener('touchend', (e) =>{
+  e.preventDefault();
+
+  console.log('End');
+});
+
+});
+
+//touches - список всіх пальців які взаємодіють
+//targetTouches - всі пальци з елементом
+//changedTouches - список пальців
