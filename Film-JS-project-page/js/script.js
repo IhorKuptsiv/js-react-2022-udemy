50d76b241501fb57aa7df2df2052c2ef42e85a0d
Ihor <cronosgothic@gmail.com>
2022-06-26 02:07:06 +0300

    remove films, add favorite, sort

--- Film-JS-project-page/js/script.js
+++ Film-JS-project-page/js/script.js
@@ -34,17 +34,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const adv = document.querySelectorAll('.promo__adv img'),
           poster = document.querySelector('.promo__bg'),
           genre = poster.querySelector('.promo__genre'),
-          movieList = document.querySelector('.promo__interactive-list');
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) =>{
+        //preventDefault -  відміняє стандартну поведінку браузера
+        // сторінка не буде обновлятись при кліці на кнопку
+       event.preventDefault();
+
+       let newFilm = addInput.value;
+       const favorite = checkbox.checked;
+
+       // якщо тру - тобто користувач щось ввів в інпут тоді кнопка активна.
+       if(newFilm){
+// берем newFilm  - і перевіряємо скільки символів там є 
+//2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+      if (newFilm.length > 21){
+        newFilm = `${newFilm.substring(0,22)}...`;
+      }
+
+      
+//4) Если в форме стоит галочка "Сделать любимым" - 
+//в консоль вывести сообщение: 
+//"Добавляем любимый фильм"
+if(favorite){
+    console.log("Добавляем любимый фильм");
+}
+
+  //Новый фильм должен добавляться в movieDB.movies.
+  movieDB.movies.push(newFilm);
+  //5) Фильмы должны быть отсортированы по алфавиту 
+  sortArr(movieDB.movies);
+
+  createMovieList(movieDB.movies, movieList);
+       }
+
     
-    adv.forEach(item =>{
+       event.target.reset();
+
+
+    });  
+    
+const deleteAdv = (arr) =>{
+    arr.forEach(item =>{
         item.remove();
     });
+};
+
     
     // adv.forEach(function (item){
     //     item.remove();
     // });
     
-    //2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+    const makeChanges = () => {
+            //2) Изменить жанр фильма, поменять "комедия" на "драма"
     //promo__genre
     genre.textContent = 'драма';
     
@@ -53,25 +99,51 @@ document.addEventListener('DOMContentLoaded', () => {
     //Реализовать только при помощи JS
     poster.style.backgroundImage = 'url("img/bg.jpg")';
     
+    };
+  
     
     //4) Список фильмов на странице сформировать на основании данных из этого JS файла.
     //Отсортировать их по алфавиту 
     //5) Добавить нумерацию выведенных фильмов */
     //очищуємо список фільмів
-    movieList.innerHTML = "";
+   
+    const sortArr = (arr) => {
+  arr.sort();
+    };
     //сортуємо по алфавіту
-    movieDB.movies.sort();
+   
     
     //всі елементи знаходяться в movieDB в свойстві movies
     // перебираємо forEach, в середину поміщаємо колбек функцію з 2ма аргументами film, i
-    movieDB.movies.forEach((film, i) => {
-       movieList.innerHTML += `
-       <li class="promo__interactive-item">${i + 1} ${film}
-         <div class="delete"></div>
-       </li>
-       `;
-    });
     
+    function createMovieList(films, parent){
+        parent.innerHTML = "";
+        sortArr(films);
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+              <div class="delete"></div>
+            </li>
+            `;
+         });
+         
 
+// клас delete , перебираємо всі через forEach 
+
+         document.querySelectorAll('.delete').forEach((btn, i) =>{
+            btn.addEventListener('click', () =>{
+             btn.parentElement.remove();
+          movieDB.movies.splice(i, 1);
+
+           createMovieList(films, parent);
+
+           });
+         });
+     
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 
 });
