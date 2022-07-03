5cb4e8edc88c763e039d725fd5fa6e36ce23e1df
Ihor <cronosgothic@gmail.com>
2022-07-03 21:47:36 +0300

    Food project website- TABS

--- js-practice2-classlist/script.js
+++ js-practice2-classlist/script.js
@@ -1,3 +1,12 @@
+
+//https://learn.javascript.ru/event-delegation
+//https://medium.com/@stasonmars/%D0%B4%D0%B5%D0%BB%D0%B5%D0%B3%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D1%81%D0%BE%D0%B1%D1%8B%D1%82%D0%B8%D0%B8%CC%86-%D0%B2-javascript-d91cbdd8916a
+//https://developer.mozilla.org/ru/docs/Web/API/Element/classList
+//https://developer.mozilla.org/ru/docs/Web/API/Element/matches
+
+
+
+
 const btns = document.querySelectorAll('button'),
       wrapper = document.querySelector('.btn-block');
 
