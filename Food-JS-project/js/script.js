99664aaf4b82b8c6f2a979a313757658529a9bea
Ihor <cronosgothic@gmail.com>
2022-08-30 21:06:36 +0300

    calculator calories

--- Food-JS-project/js/script.js
+++ Food-JS-project/js/script.js
@@ -653,7 +653,88 @@ dots[slideIndex - 1].style.opacity = 1;
 });
 });
 
+//----------------------------Calc
 
+const result = document.querySelector('.calculating__result span');
+// фільтри
+let sex = 'female', 
+height, weight, age, 
+ratio=1.375;
 
+// функцію яку будемо викликати 
+function calcTotal() {
+  // якщо всі фільтри вибрані
+  //хочаб 1 фільтр не вибрано - false і пишемо повідомлення
+  if( !sex || !height || !weight || !age || !ratio){
+    // якщо хоч 1 фільтр відсутній, пишемо повідомлення
+    result.textContent = '____';
+    return;// щоб перервати достроково функцію
+  }
+  if (sex === 'female') {
+    // Math.round -  округляэмо числа
+    result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+} else {
+    result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+}
+ 
+}
+calcTotal();
+
+// получаємо статичну інфу
+function getStaticInformation(parentSelector, activeClass){
+  //отримуємо всі div
+ const elements = document.querySelectorAll(`${parentSelector} div`);
+  
+ // elements.forEach(elem => { -   виправляємо баг з кліком між кнопками калькулятора
+ elements.forEach(elem => {
+  elem.addEventListener('click',(e) => {
+    //якщо користувач вибрав  фізичну активність
+     if(e.target.getAttribute('data-ratio')){
+       ratio = +e.target.getAttribute('data-ratio');
+     }else{
+       sex = e.target.getAttribute('id');
+     }
+    // console.log(ratio,sex);
+     
+     // перевіряємо активні кнопки
+     elements.forEach(elem => {
+     // позбавляємо активності всі кнопки
+     elem.classList.remove(activeClass);
+     });
+     e.target.classList.add(activeClass);
+     calcTotal();
+   });
+ });
+
+ //document.querySelector(parentSelector)
+}
+getStaticInformation('#gender','calculating__choose-item_active');
+getStaticInformation('.calculating__choose_big','calculating__choose-item_active');
+
+// функціяю обробляє кожен окремий інпут
+ function getDynamicInformation (selector){
+  const input = document.querySelector(selector);
+  // оброботчик подій
+  input.addEventListener('input', () => {
+   switch(input.getAttribute('id')){
+    case 'height':
+      height= +input.value;
+      break;
+    case 'weight':
+      weight = +input.value;
+      break;
+    case 'age':
+      age = +input.value;
+      break;
+   }
+   calcTotal();
+  });
+ 
+ }
+
+ getDynamicInformation('#height');
+ getDynamicInformation('#weight');
+ getDynamicInformation('#age');
 });
+
 
