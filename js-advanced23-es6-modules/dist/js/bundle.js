2a9f071f7019b09446a1ff6847e97cbb58d5395a
Ihor <cronosgothic@gmail.com>
2022-09-13 22:21:35 +0300

    ES6 Modules

---
+++ js-advanced23-es6-modules/dist/js/bundle.js
@@ -0,0 +1,70 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/script.js":
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _script__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./script */ "./src/js/script.js");
+//імпортуємо з main
+
+
+
+// використовуєм імпортоване
+console.log(`${_script__WEBPACK_IMPORTED_MODULE_0__.one} and ${_script__WEBPACK_IMPORTED_MODULE_0__.two}`);
+// але цей код потрібно ЗБИРАТИ зборщиком модулів !!!
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/script.js");
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ js-advanced23-es6-modules/src/js/main.js
@@ -0,0 +1,15 @@
+//експортуємо з main в script
+
+//варіант 1. експортуємо змінну one
+export let one = 1;
+
+//варіант 2. експортуємо змінну two
+let two = 2;
+export {
+    two
+};
+
+//варіант 3. експорт функції
+export function sayHi() {
+    console.log('Hello');
+}
