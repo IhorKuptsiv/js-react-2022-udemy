5cf752e8d63bd1649459c3040d6756811c757166
Ihor <cronosgothic@gmail.com>
2022-09-23 13:02:38 +0300

    jQuery. ready,first,toggle,even,odd,eq,on

--- js-advanced30-jquery/script.js
+++ js-advanced30-jquery/script.js
@@ -4,5 +4,32 @@ import $ from 'jquery';
 //в jQuery   $('#btn');
 //$ це функція по отриманню елемента
 //$('#btn');
-const btn = $('#btn');
-console.log(btn);
+//const btn = $('#btn');
+//console.log(btn);
+
+//DOM content loaded  --- JS
+//jquery -- $(document).ready()
+$(document).ready(function () {
+    //при наведені на 1шу кнопку буде змінюватись клас активності
+    //з list-item на active з анімацією toggleClass
+    //:first - перший елемент - jquery
+    $('.list-item:first').hover(function () {
+        $(this).toggleClass('active');
+    });
+    //скриваємо всі парні зображення при кліці
+    //eq 2 -третій елемент
+    //on - оброботчик подій
+    $('.list-item:eq(2)').on('click', function () {
+        //парні зображення
+        $('.image:even').fadeToggle('slow');
+    });
+
+    //анімація вручну, до 5 кнопки( eq4)
+    $('.list-item:eq(4)').on('click', function () {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 2000);
+    });
+
+});
