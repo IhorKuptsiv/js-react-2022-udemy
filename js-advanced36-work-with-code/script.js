eb39a3aada4616779572ef2b7ed842841b1df696
Ihor <cronosgothic@gmail.com>
2022-09-27 14:11:20 +0300

    work with finished code

--- js-advanced36-work-with-code/script.js
+++ js-advanced36-work-with-code/script.js
@@ -4,3 +4,15 @@
 
 //слайдер: slick, own carousel
 //git : tiny-slider
+
+import {
+    tns
+} from "./node_modules/tiny-slider/src/tiny-slider";
+
+
+tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true
+});
